5b081675ef1b3b8b41f5297fbf613d110fe8c285
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-28 00:11:27 -0600

    chore: add cache to homepage

--- apps/next-rainbow/app/page.tsx
+++ apps/next-rainbow/app/page.tsx
@@ -1,5 +1,7 @@
 import { MarketsTracker } from '@repo/markets'
 
+export const revalidate = 300 // 5 minutes
+
 export default function HomePage() {
   return (
     <div className="container mx-auto">
